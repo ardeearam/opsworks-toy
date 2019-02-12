c431c9aff139eada6593793942958911dc4fa27d
Ardee Aram <ardee.aram@gmail.com>
2019-02-12 08:30:06 +0000

    This is a toy.

---
+++ README.md
@@ -0,0 +1,4 @@
+# balsam
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook:: balsam
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+#
+#
+apt_update
+
+apt_package %w(nodejs python3 ruby)
+
+file "/home/ardee/hello.txt" do
+  content "Hello, this is my first cookbook recipe\n"
+  action :create
+end
+
+execute "hello_message" do
+  command "ls -l > /home/ardee/directory.txt"
+end
+
